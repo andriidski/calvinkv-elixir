74e16fb7e591376f264d785261635cf9a58ba31f
andriidski <andriidski@proton.me>
2020-11-19 18:01:28 -0500

    ran mix format on tests

--- apps/calvin/test/test_sequencer.exs
+++ apps/calvin/test/test_sequencer.exs
@@ -1,48 +1,56 @@
 defmodule SequencerTest do
-	use ExUnit.Case
-    doctest Sequencer
-    
-	import Emulation, only: [spawn: 2, send: 2]
-	import Kernel,
-		except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
+  use ExUnit.Case
+  doctest Sequencer
 
-	test "Requests to the Sequencer component are logged" do
-		Emulation.init()
-		Emulation.append_fuzzers([Fuzzers.delay(2)])
+  import Emulation, only: [spawn: 2, send: 2]
 
-		# default replica group and partition single it's only a single node
-		sequencer_proc = Sequencer.new(:A, 1)
-		sequencer_proc_id = Component.get_id(sequencer_proc)
+  import Kernel,
+    except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
 
-		IO.puts("created a Sequencer component: #{inspect(sequencer_proc)} with id: #{sequencer_proc_id}")
+  test "Requests to the Sequencer component are logged" do
+    Emulation.init()
+    Emulation.append_fuzzers([Fuzzers.delay(2)])
 
-		# start the node
-		spawn(sequencer_proc_id, fn -> Sequencer.start(sequencer_proc) end)
+    # default replica group and partition single it's only a single node
+    sequencer_proc = Sequencer.new(:A, 1)
+    sequencer_proc_id = Component.get_id(sequencer_proc)
 
-		client = spawn(:client,
-			fn -> 
-				client = Client.connect_to(sequencer_proc_id)
+    IO.puts(
+      "created a Sequencer component: #{inspect(sequencer_proc)} with id: #{
+        sequencer_proc_id
+      }"
+    )
 
-				# test a ping request to the Sequencer
-				Client.ping_sequencer(client)
-			end
-		)
+    # start the node
+    spawn(sequencer_proc_id, fn -> Sequencer.start(sequencer_proc) end)
 
-		# wait for a bit for all requests to be logged
-		wait_timeout = 1000
-		receive do
-		after
-			wait_timeout -> :ok
-		end
+    client =
+      spawn(
+        :client,
+        fn ->
+          client = Client.connect_to(sequencer_proc_id)
 
-		handle = Process.monitor(client)
+          # test a ping request to the Sequencer
+          Client.ping_sequencer(client)
+        end
+      )
+
+    # wait for a bit for all requests to be logged
+    wait_timeout = 1000
+
+    receive do
+    after
+      wait_timeout -> :ok
+    end
+
+    handle = Process.monitor(client)
     # timeout
     receive do
       {:DOWN, ^handle, _, _, _} -> true
     after
       30_000 -> false
     end
-	after
-			Emulation.terminate()
-	end
+  after
+    Emulation.terminate()
+  end
 end
--- apps/calvin/test/test_storage.exs
+++ apps/calvin/test/test_storage.exs
@@ -1,55 +1,63 @@
 defmodule StorageTest do
-	use ExUnit.Case
-    doctest Storage
-    
-	import Emulation, only: [spawn: 2, send: 2]
-	import Kernel,
-		except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
+  use ExUnit.Case
+  doctest Storage
 
-	test "Commands to the Storage component are logged" do
-		Emulation.init()
-		Emulation.append_fuzzers([Fuzzers.delay(2)])
+  import Emulation, only: [spawn: 2, send: 2]
 
-		# default replica group and partition single it's only a single node
-		storage_proc = Storage.new(:A, 1)
-		storage_proc_id = Component.get_id(storage_proc)
+  import Kernel,
+    except: [spawn: 3, spawn: 1, spawn_link: 1, spawn_link: 3, send: 2]
 
-		IO.puts("created a Storage component: #{inspect(storage_proc)} with id: #{storage_proc_id}")
+  test "Commands to the Storage component are logged" do
+    Emulation.init()
+    Emulation.append_fuzzers([Fuzzers.delay(2)])
 
-		# start the node
-		spawn(storage_proc_id, fn -> Storage.start(storage_proc) end)
+    # default replica group and partition single it's only a single node
+    storage_proc = Storage.new(:A, 1)
+    storage_proc_id = Component.get_id(storage_proc)
 
-		client = spawn(:client,
-			fn -> 
-				client = Client.connect_to(storage_proc_id)
+    IO.puts(
+      "created a Storage component: #{inspect(storage_proc)} with id: #{
+        storage_proc_id
+      }"
+    )
 
-				# perform some operations
-				# create a -> 1
-				Client.create(client, :a, 1)
-				# create b -> 2
-				Client.create(client, :b, 2)
-				# update a -> 2
-				Client.update(client, :a, 2)
-				# delete b
-				Client.delete(client, :b)
-			end
-		)
+    # start the node
+    spawn(storage_proc_id, fn -> Storage.start(storage_proc) end)
 
-		# wait for a bit for all requests to be logged
-		wait_timeout = 1000
-		receive do
-		after
-			wait_timeout -> :ok
-		end
+    client =
+      spawn(
+        :client,
+        fn ->
+          client = Client.connect_to(storage_proc_id)
 
-		handle = Process.monitor(client)
+          # perform some operations
+          # create a -> 1
+          Client.create(client, :a, 1)
+          # create b -> 2
+          Client.create(client, :b, 2)
+          # update a -> 2
+          Client.update(client, :a, 2)
+          # delete b
+          Client.delete(client, :b)
+        end
+      )
+
+    # wait for a bit for all requests to be logged
+    wait_timeout = 1000
+
+    receive do
+    after
+      wait_timeout -> :ok
+    end
+
+    handle = Process.monitor(client)
     # timeout
     receive do
       {:DOWN, ^handle, _, _, _} -> true
     after
       30_000 -> false
     end
-	after
-			Emulation.terminate()
-	end
+  after
+    Emulation.terminate()
+  end
 end
