35fc56e0b7659bcdde328a823b722f208178dc1d
andriidski <andriidski@proton.me>
2020-11-30 18:12:27 -0500

    generating partition map of key to partition number

--- apps/calvin/lib/partitioning.ex
+++ apps/calvin/lib/partitioning.ex
@@ -4,19 +4,61 @@
 defmodule PartitionScheme do
   alias __MODULE__
 
-  @enforce_keys [:num_partitions]
+  @enforce_keys [:num_partitions, :partition_map]
 
   defstruct(
-    num_partitions: nil
+    num_partitions: nil,
+    # mapping of key to partition which the key is assigned to
+    partition_map: nil
   )
+
+  @doc """
+  Generates a key partition map given `num_partitions` of partitions. This maps 
+  each value in a key range from a -> z to an assigned partition in the range [1, num_partitions],
+  and in a way 'chunks' the key range such that it is split between partitions. 
+
+  For example, given `num_partitions` = 2, the key range of [a ... z] is first partitioned into
+  [[a ... m], [n ... z]]. Then it is flat mapped into a list of maps in form of 
+  [{a->1}, {b->1} ... {z->2}] with keys a-m assigned to partition 1 and n-z assigned to partition 2.
+  Then the maps are combined into a single map of {key -> partition} such that for each key in the 
+  range of a->z, based on how many partitions we have, we have a corresponding partition assigned
+  """
+  @spec generate_key_partition_map(non_neg_integer()) :: %{}
+  def generate_key_partition_map(num_partitions) do
+    # generate a range of keys from a -> z
+    key_range = Enum.map(0..25, fn n -> List.to_atom([n + 97]) end)
+
+    # partition the key range such that we have `num_partitions` chunks
+    key_range_partitioned = Enum.chunk_every(key_range, ceil(26 / num_partitions))
+  
+    # index each chunk, then map each chunk such that each key in the chunk
+    # is paired with the index of the chunk. This correspondings to assigning
+    # a partition number to each value in the key range and wil result in multiple
+    # maps which are then flattened into a single list
+    key_maps = Enum.flat_map(Enum.with_index(key_range_partitioned), 
+      fn {range, idx} -> 
+        Enum.map(range, 
+          fn val -> 
+            count = %{}
+            Map.put(count, val, idx+1)
+          end) 
+      end
+    )
+    # reduce the maps of key -> partition into a single map
+    Enum.reduce(key_maps, %{}, fn map, acc -> Map.merge(acc, map) end)
+  end
+
 
   @doc """
   Creates a new PartitionScheme with `num_partitions` partitions per replica
   """
   @spec new(non_neg_integer()) :: %PartitionScheme{}
   def new(num_partitions) do
+    partition_map = PartitionScheme.generate_key_partition_map(num_partitions)
+    
     %PartitionScheme{
       num_partitions: num_partitions,
+      partition_map: partition_map
     }
   end
 
